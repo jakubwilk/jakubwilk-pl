6c138df2ce1175497107c8f4de066d40fdc9fc75
jakubwilk <jw@jakubwilk.pl>
2024-01-10 21:31:37 +0100

    Update css and change imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,13 @@
 'use client'
 import { useEffect } from 'react'
-import Header from '@components/Header'
-import SocialsGrid from '@components/SocialsGrid'
+import { GamesSection } from '@components/games/GamesSection'
+import Header from '@components/header/Header'
+import { ProfileSocials } from '@components/ProfileSocials'
 import { ThemeTypesEnum } from '@enums/ThemeEnums'
 import { useThemeContext } from '@hooks/useThemeContext'
-import { useTranslation } from '@hooks/useTranslation'
 
 export default function HomePage() {
   const { theme } = useThemeContext()
-  const { t } = useTranslation()
 
   useEffect(() => {
     if (theme === ThemeTypesEnum.LIGHT) {
@@ -26,12 +25,8 @@ export default function HomePage() {
       <main
         className={'flex flex-col items-center justify-center min-h-screen py-24 px-4'}
       >
-        <h1 className={'pb-4 title'}>
-          {`👋 ${t('greetings')} `}
-          <span>{t('name')}</span>
-        </h1>
-        <p className={'description'}>{t('description')}</p>
-        <SocialsGrid />
+        <ProfileSocials />
+        <GamesSection />
       </main>
     </div>
   )
