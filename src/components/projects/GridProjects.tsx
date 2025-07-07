fb29471e8dd6fa5ab8fe3ca67ea5b55c82274619
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2025-07-07 15:50:06 +0200

    Add project to grid

--- src/components/projects/GridProjects.tsx
+++ src/components/projects/GridProjects.tsx
@@ -1,11 +1,16 @@
 import { IProject } from 'models'
+import ItemProject from './ItemProject'
 
 interface IGridProjectsProps {
   projects: IProject[]
 }
 
 export default function GridProjects({ projects }: IGridProjectsProps) {
-  console.log('projects', projects)
-
-  return <div>GridProjects</div>
+  return (
+    <div className='grid grid-cols-1 gap-4 lg:grid-cols-2'>
+      {projects.map((project) => (
+        <ItemProject key={project.id} project={project} />
+      ))}
+    </div>
+  )
 }
--- src/components/projects/ItemProject.tsx
+++ src/components/projects/ItemProject.tsx
@@ -1,3 +1,47 @@
-export default function ItemProject() {
-  return <div>ItemProject</div>
+import {
+  Badge,
+  Button,
+  Card,
+  CardSection,
+  Divider,
+  Group,
+  Image,
+  Text,
+} from '@mantine/core'
+import { IProject } from 'models'
+
+interface IItemProjectProps {
+  project: IProject
+}
+
+export default function ItemProject({ project }: IItemProjectProps) {
+  return (
+    <Card padding='lg' withBorder>
+      <CardSection>
+        <Image src={project.image} alt={project.name} height={200} />
+      </CardSection>
+      <Divider my='md' />
+      <div className='flex flex-col gap-2 mb-4'>
+        <Text size='lg' fw={500}>
+          {project.name}
+        </Text>
+        <div className='flex flex-wrap gap-2'>
+          {project.technologies.map((technology) => (
+            <Badge key={technology}>{technology}</Badge>
+          ))}
+        </div>
+      </div>
+      <Text>{project.description}</Text>
+      <Button
+        fullWidth
+        radius={0}
+        mt='md'
+        component='a'
+        href={project.link}
+        target='_blank'
+      >
+        {project.name + ' on GitHub'}
+      </Button>
+    </Card>
+  )
 }
