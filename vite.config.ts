bbd5abfb18d562105281555bf528ff78d084a22d
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2025-07-07 14:11:36 +0200

    Install router and setup tsconfig with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: 'react',
+    }),
+  ],
   resolve: {
     alias: {
       components: path.resolve('src/components'),
+      pages: path.resolve('src/pages'),
     },
   },
 })
